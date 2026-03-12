c3e02cde3cccdf026f0d847b6248475f8880d5ac
nicolaszbq <nicolascontatotrabalho@gmail.com>
2026-03-12 17:43:17 -0300

    Implemented a connection to dashboard/userArea at the initial page, based on the user ROLE

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const buttonUserArea = document.getElementById("userArea");
+    const buttonDashboard = document.getElementById("trainerArea");
+    const loginButton = document.getElementById("loginBtn");
+    const registerButton = document.getElementById("registerBtn");
 
-    const user = localStorage.getItem("user");
+    if (buttonUserArea && buttonDashboard) {
+        const userRaw = localStorage.getItem("user");
+        const user = JSON.parse(userRaw);
+        console.log("USER RAW: "+ userRaw);
+        console.log("USER CONVERTED: "+ user);
 
-    if (user != null) {
-        
-        const buttonUserArea = document.getElementById("userArea");
-        const buttonDashboard = document.getElementById("dashboard");
-
-        buttonUserArea.style.display = "none";
-        buttonDashboard.style.display = "block";
+        if(userRaw){
+            loginButton.style.display = "none";
+            registerButton.style.display = "none";
+            const role = user.role;
+            if(role == "TRAINER"){
+                console.log("O USUARIO É TREINADOR");
+                console.log(role);
+                buttonUserArea.style.display = "none";
+                buttonDashboard.style.display = "block";
+            }else if(role == "MEMBER"){
+                console.log("O USUARIO É MEMBRO");
+                console.log(role);
+                buttonUserArea.style.display = "block";
+                buttonDashboard.style.display = "none";
+            }else{
+                console.log("O USUARIO É ADMIN")
+                console.log(role);
+            }
+        }else{
+            buttonDashboard.style.display = "none";
+            buttonUserArea.style.display = "none";
+            console.log("NENHUM USUARIO LOGGADO");
+        }
+    } else {
+      console.error("Elementos não encontrados no DOM.");
     }
-
 });
